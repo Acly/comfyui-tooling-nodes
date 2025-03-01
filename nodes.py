93e19324560013908bf8ae4f07f2146e662a3ae4
Acly <aclysia@gmail.com>
2025-03-01 20:20:24 +0100

    Don't return alpha channel from LoadImageBase64 inverted ... why was it ever inverted?

--- nodes.py
+++ nodes.py
@@ -28,9 +28,9 @@ class LoadImageBase64:
 
         if "A" in img.getbands():
             mask = np.array(img.getchannel("A")).astype(np.float32) / 255.0
-            mask = 1.0 - torch.from_numpy(mask)
+            mask = torch.from_numpy(mask)
         else:
-            mask = torch.zeros((64, 64), dtype=torch.float32, device="cpu")
+            mask = None
 
         img = img.convert("RGB")
         img = np.array(img).astype(np.float32) / 255.0
